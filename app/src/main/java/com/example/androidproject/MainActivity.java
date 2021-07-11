d221a086bf72753ad2b4f544c141ea6f7668257e
ytr100 <tamir1311@gmail.com>
2021-07-11 17:46:43 +0300

    initial commit home, login, signup screens

---
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.androidproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.Navigation;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ app/src/main/java/com/example/androidproject/signup.java
@@ -0,0 +1,69 @@
+package com.example.androidproject;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link signup#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class signup extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public signup() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment signup.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static signup newInstance(String param1, String param2) {
+        signup fragment = new signup();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_signup, container, false);
+        Button btn = view.findViewById(R.id.signup_btn);
+        btn.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_signup_to_login));
+        return view;
+    }
+}
