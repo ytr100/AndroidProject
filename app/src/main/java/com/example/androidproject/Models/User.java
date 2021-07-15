856fd741e0a72d3ce4cca9e895cc987a2b73d42a
RoninEMH <60003921+RoninEMH@users.noreply.github.com>
2021-07-15 21:36:26 +0300

    updating user and gradle

--- app/src/main/java/com/example/androidproject/Models/User.java
+++ app/src/main/java/com/example/androidproject/Models/User.java
@@ -1,10 +1,15 @@
 package com.example.androidproject.Models;
 
-import android.graphics.Bitmap;
+import android.content.Context;
+import android.content.SharedPreferences;
 
 import androidx.annotation.NonNull;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
+
+import com.example.androidproject.MyApplication;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.FieldValue;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,31 +18,49 @@ import java.util.Map;
 public class User {
 
     final private static String USERNAME = "username";
-    final private static String PASSWORD = "password";
     final private static String EMAIL = "email";
     final private static String PHOTO = "photo";
+    final private static String LAST_UPDATED = "lastUpdated";
+
+    final private static String LOCAL_LAST_UPDATED = "userLastUpdated";
+
     @PrimaryKey
     @NonNull
     private String username;
-    private String password;
     private String email;
     private String photo;
+    private Long lastUpdated;
 
-    static public User fromJson(Map<String, Object> json) {
+
+    public static void setLocal_lastUpdated(Long ts) {
+        SharedPreferences.Editor editor = MyApplication.context.getSharedPreferences("TAG", Context.MODE_PRIVATE).edit();
+        editor.putLong(LOCAL_LAST_UPDATED, ts);
+        editor.commit();
+    }
+
+    public static Long getLocal_lastUpdated(Long ts) {
+        return MyApplication.context.getSharedPreferences("TAG", Context.MODE_PRIVATE).getLong(LOCAL_LAST_UPDATED, 0);
+    }
+
+    public User fromJson(Map<String, Object> json) {
         User user = new User();
         user.setUsername((String) json.get(USERNAME));
-        user.setPassword((String) json.get(PASSWORD));
         user.setEmail((String) json.get(EMAIL));
         user.setPhoto((String) json.get(PHOTO));
+        Timestamp ts = (Timestamp) json.get(LAST_UPDATED);
+        if (ts != null)
+            user.setLastUpdated(ts.getSeconds());
+        else
+            user.setLastUpdated(0L);
         return user;
     }
 
     public Map<String, Object> toJson() {
         Map<String, Object> json = new HashMap<>();
         json.put(USERNAME, this.username);
-        json.put(PASSWORD, this.password);
         json.put(EMAIL, this.email);
         json.put(PHOTO, this.photo);
+        json.put(LAST_UPDATED, FieldValue.serverTimestamp());
         return json;
     }
 
@@ -48,14 +71,6 @@ public class User {
 
     public void setUsername(@NonNull String username) {
         this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public String getEmail() {
@@ -73,4 +88,13 @@ public class User {
     public void setPhoto(String photo) {
         this.photo = photo;
     }
+
+    public Long getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Long lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
 }
